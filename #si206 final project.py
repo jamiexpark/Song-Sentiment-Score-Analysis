59b313dbc225fd7f76d2c6313fe6614e2f3c0e54
LMcGart <92118030+LMcGart@users.noreply.github.com>
2023-04-20 20:46:08 -0400

    Updated sentiment function

--- #si206 final project.py
+++ #si206 final project.py
@@ -78,7 +78,7 @@ def parse_web_with_soup(website):
       streams = cols[3].text.strip()
       artist_streams.append((artist, streams))
       counter += 1
-      
+    
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS artists(
             name TEXT,
@@ -117,7 +117,7 @@ def top_ten_songs(artist_streams):
 
 
     #need help getting artist id
-    list_artist = ('3TVXtAsR1Inumwj472S9r4','4q3ewBCX7sLwd24euuV69X','06HL4z0CvFAxyc27GXpf02','6eUKZXaKkcviH0Ku9w2n3V','1Xyo4u8uXC1ZmMpatF05PJ','1uNFoZAHBGtllmzznpCI3s','66CXWjxzNUsdJxJ2JdwvnR','7dGJo4pcD2V6oG8kP0tJRR','3Nrfpe0tUJi4K4DXYWgMUX','246dkjvS1zLTtiykXe5h60')
+    list_artist = ('3TVXtAsR1Inumwj472S9r4','4q3ewBCX7sLwd24euuV69X','06HL4z0CvFAxyc27GXpf02','1Xyo4u8uXC1ZmMpatF05PJ', '6eUKZXaKkcviH0Ku9w2n3V', '1uNFoZAHBGtllmzznpCI3s','66CXWjxzNUsdJxJ2JdwvnR','7dGJo4pcD2V6oG8kP0tJRR','3Nrfpe0tUJi4K4DXYWgMUX','246dkjvS1zLTtiykXe5h60')
 
     total_top10_songs = []
     indiv_top10_songs = []
@@ -157,32 +157,13 @@ def related_artists(artist_streams):
         for artist in re['artists']:
             print('  ', artist['name'])
 
-
-# c.execute("""CREATE TABLE IF NOT EXISTS sentiments(
-#             song TEXT,
-#             sentiment_score TEXT
-#             )""")
-
 def top_song_verses(top_10_songs):
     api_key = "DtKTvEx0TjZXb3MJt0DIY_6HmKi-jSrCMuYngYO1LJpk0UTlGdOGBgjiEokMruJz"
     genius = Genius(api_key, skip_non_songs=True, remove_section_headers=True)
     genius.timeout = 120
 
-    # sentiment_pipeline = pipeline("sentiment-analysis")
     sentiment = SentimentIntensityAnalyzer()
 
-    # artist = genius.search_artist("Drake", max_songs=0)
-    # song123 = genius.search_song("WAIT FOR U", artist.name)
-    # print(song123.lyrics)
-
-
-    # artist.add_song(song)
-    # artist.add_song("Rich Flex")
-    # artist.save_lyrics()
-
-    # print(songs)
-    # song = artist.song("Rich Flex")
-    # print(song.lyrics)
     drake_list = []
     bad_bunny_list = []
     taylor_swift_list = []
@@ -197,7 +178,7 @@ def top_song_verses(top_10_songs):
 
     index = 1
     pattern = r'^(.*?)(\s*(\(|\[|\-).*)?$'
-    # pattern = r'^([\w\s]+)(?:(?:\s-\s)|(?:\s\(|\[))(?:.*)'
+
     for artist, songs in top_10_songs:
         for song in songs:
             if song == "Anti-Hero":
@@ -229,16 +210,16 @@ def top_song_verses(top_10_songs):
                 elif index > 90 and index <= 100:
                     post_malone_list.append(match.group(1))
             index += 1
-    # print(drake_list)
-    # print(bad_bunny_list)
-    # print(taylor_swift_list)
-    # print(the_weeknd_list)
-    # print(ed_sheeran_list)
-    # print(justin_bieber_list)
-    # print(ariana_grande_list)
-    # print(eminem_list)
-    # print(bts_list)
-    # print(post_malone_list)
+
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS sentiments(
+            song TEXT,
+            sentiment_score TEXT
+            )""")
+    
+    c.execute("SELECT COUNT(*) FROM sentiments")
+    row_count = c.fetchone()[0]
+    print(row_count)
     drake_lyric_list = []
     bad_bunny_lyric_list = []
     taylor_swift_lyric_list = []
@@ -249,89 +230,209 @@ def top_song_verses(top_10_songs):
     eminem_lyric_list = []
     bts_lyric_list = []
     post_malone_lyric_list = []
-    # print(drake_lyric_list)
-    for song in drake_list:
-        artist = genius.search_artist("Drake", max_songs=0)
-        song1 = genius.search_song(song, artist.name)
-        lyrics = song1.lyrics
-        # clean_lyrics = re.sub(r'^.*?(?=\n\n)|\\n|Translations.*$|\d+', '', lyrics, flags=re.DOTALL)
-        clean_lyrics = lyrics.replace("\n", " ")
-        clean_lyrics = clean_lyrics.replace("Embed", "")
-        clean_lyrics = clean_lyrics.replace("\\", "")
-        drake_lyric_list.append(clean_lyrics)
-    
-    print(drake_lyric_list)
-    # for song in bad_bunny_list:
-    #     artist = genius.search_artist("Bad Bunny", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     bad_bunny_lyric_list.append(song1.lyrics)
 
-    # for song in taylor_swift_list:
-    #     artist = genius.search_artist("Taylor Swift", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     taylor_swift_lyric_list.append(song1.lyrics)
-
-    # for song in the_weeknd_list:
-    #     artist = genius.search_artist("The Weeknd", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     the_weeknd_lyric_list.append(song1.lyrics)
-
-    # for song in ed_sheeran_list:
-    #     artist = genius.search_artist("Ed Sheeran", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     ed_sheeran_lyric_list.append(song1.lyrics)
-
-    # for song in justin_bieber_list:
-    #     artist = genius.search_artist("Justin Bieber", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     justin_bieber_lyric_list.append(song1.lyrics)
-
-    # for song in ariana_grande_list:
-    #     artist = genius.search_artist("Ariana Grande", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     ariana_grande_lyric_list.append(song1.lyrics)
-
-    # for song in eminem_list:
-    #     artist = genius.search_artist("Eminem", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     eminem_lyric_list.append(song1.lyrics)
-
-    # for song in bts_list:
-    #     artist = genius.search_artist("BTS", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     bts_lyric_list.append(song1.lyrics)
-
-    # for song in post_malone_list:
-    #     artist = genius.search_artist("Post Malone", max_songs=0)
-    #     song1 = genius.search_song(song, artist.name)
-    #     post_malone_lyric_list.append(song1.lyrics)
-   
-    # print(sentiment_pipeline(drake_lyric_list))
-    # sentiment_pipeline(drake_lyric_list)
     drake_sent = []
-    for drake_song in drake_lyric_list:
-        sents = sentiment.polarity_scores(drake_song)
-        drake_sent.append(sents['compound'])
-    drake_tups = list(zip(drake_list, drake_sent))
-    c = conn.cursor()
-    c.execute("""CREATE TABLE IF NOT EXISTS sentiments(
-            song TEXT,
-            sentiment_score TEXT
-            )""")
-    c.executemany("INSERT INTO sentiments VALUES (?, ?)", drake_tups)
+    bad_sent = []
+    tay_sent = []
+    week_sent = []
+    ed_sent = []
+    justin_sent = []
+    ariana_sent = []
+    em_sent = []
+    bts_sent = []
+    post_sent = []
 
+    if row_count == 100:
+        return
+    else:
+        if row_count == 0:
+            for song in drake_list:
+                artist = genius.search_artist("Drake", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                drake_lyric_list.append(clean_lyrics)
 
-    conn.commit()
+            for drake_song in drake_lyric_list:
+                sents = sentiment.polarity_scores(drake_song)
+                drake_sent.append(sents['compound'])
+
+            drake_tups = list(zip(drake_list, drake_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", drake_tups)
+            conn.commit()
+
+        elif row_count == 10:
+            for song in bad_bunny_list:
+                artist = genius.search_artist("Bad Bunny", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                bad_bunny_lyric_list.append(clean_lyrics)
+
+            for bad_song in bad_bunny_lyric_list:
+                sents = sentiment.polarity_scores(bad_song)
+                bad_sent.append(sents['compound'])
+
+            bad_tups = list(zip(bad_bunny_list, bad_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", bad_tups)
+            conn.commit()
+
+        elif row_count == 20:
+            for song in taylor_swift_list:
+                artist = genius.search_artist("Taylor Swift", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                taylor_swift_lyric_list.append(clean_lyrics)
+            
+            for tay_song in taylor_swift_lyric_list:
+                sents = sentiment.polarity_scores(tay_song)
+                tay_sent.append(sents['compound'])
+
+            tay_tups = list(zip(taylor_swift_list, tay_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", tay_tups)
+            conn.commit()
+
+        elif row_count == 30:
+            for song in the_weeknd_list:
+                artist = genius.search_artist("The Weeknd", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                the_weeknd_lyric_list.append(clean_lyrics)
+
+            for week_song in the_weeknd_lyric_list:
+                sents = sentiment.polarity_scores(week_song)
+                week_sent.append(sents['compound'])
+
+            week_tups = list(zip(the_weeknd_list, week_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", week_tups)
+            conn.commit()
+
+        elif row_count == 40:
+            for song in ed_sheeran_list:
+                artist = genius.search_artist("Ed Sheeran", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                ed_sheeran_lyric_list.append(clean_lyrics)
+
+            for ed_song in ed_sheeran_lyric_list:
+                sents = sentiment.polarity_scores(ed_song)
+                ed_sent.append(sents['compound'])
+
+            ed_tups = list(zip(ed_sheeran_list, ed_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", ed_tups)
+            conn.commit()
+
+        elif row_count == 50:
+            for song in justin_bieber_list:
+                artist = genius.search_artist("Justin Bieber", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                justin_bieber_lyric_list.append(song1.lyrics)
+
+            for justin_song in justin_bieber_lyric_list:
+                sents = sentiment.polarity_scores(justin_song)
+                justin_sent.append(sents['compound'])
+
+            justin_tups = list(zip(justin_bieber_list, justin_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", justin_tups)
+            conn.commit()
+                
+        elif row_count == 60:
+            for song in ariana_grande_list:
+                artist = genius.search_artist("Ariana Grande", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                ariana_grande_lyric_list.append(clean_lyrics)
+
+            for ariana_song in ariana_grande_lyric_list:
+                sents = sentiment.polarity_scores(ariana_song)
+                ariana_sent.append(sents['compound'])
+
+            ariana_tups = list(zip(ariana_grande_list, ariana_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", ariana_tups)
+            conn.commit()
+
+        elif row_count == 70:
+            for song in eminem_list:
+                artist = genius.search_artist("Eminem", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                eminem_lyric_list.append(clean_lyrics)
+
+            for em_song in eminem_lyric_list:
+                sents = sentiment.polarity_scores(em_song)
+                em_sent.append(sents['compound'])
+
+            em_tups = list(zip(eminem_list, em_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", em_tups)
+            conn.commit()
+
+        elif row_count == 80:
+            for song in bts_list:
+                artist = genius.search_artist("BTS", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                bts_lyric_list.append(song1.lyrics)
+
+            for bts_song in bts_lyric_list:
+                sents = sentiment.polarity_scores(bts_song)
+                bts_sent.append(sents['compound'])
+
+            bts_tups = list(zip(bts_list, bts_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", bts_tups)
+            conn.commit()
+
+        elif row_count == 90:
+            for song in post_malone_list:
+                artist = genius.search_artist("Post Malone", max_songs=0)
+                song1 = genius.search_song(song, artist.name)
+                lyrics = song1.lyrics
+                clean_lyrics = lyrics.replace("\n", " ")
+                clean_lyrics = clean_lyrics.replace("Embed", "")
+                clean_lyrics = clean_lyrics.replace("\\", "")
+                post_malone_lyric_list.append(clean_lyrics)
+
+            for post_song in post_malone_lyric_list:
+                sents = sentiment.polarity_scores(post_song)
+                post_sent.append(sents['compound'])
+
+            post_tups = list(zip(post_malone_list, post_sent))
+            c.executemany("INSERT INTO sentiments VALUES (?, ?)", post_tups)
+            conn.commit()
+
     conn.close()
-    # print(drake_lyric_list)
-
-
 
 def main():
     top_artists = parse_web_with_soup("https://chartmasters.org/most-streamed-artists-ever-on-spotify/")
     # print(top_artists)
     # top_artists
     top_songs = top_ten_songs(top_artists)
+    # print(top_songs)
     top_song_verses(top_songs)
     # print(top_songs)
 
